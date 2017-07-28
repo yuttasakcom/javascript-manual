3e83b2bb8aa5497a3436e88113f527870eff72f5
YoProgrammer <yuttasakcom@gmail.com>
2017-07-28 22:34:44 +0700

    Add example callback

---
+++ example/callback/index.js
@@ -0,0 +1,30 @@
+function Open() {
+  console.log('1. Open The Door')
+}
+
+function Order() {
+  console.log('2. Order a Coffee')
+}
+
+function Sit() {
+  console.log('3. Sitdown')
+}
+
+function Drink() {
+  console.log('4. Drink Coffee')
+}
+
+function Pay() {
+  console.log('5. Pay Money')
+}
+
+function Exit() {
+  console.log('6. Exit Shop')
+}
+
+Open()
+Order()
+Sit()
+Drink()
+Pay()
+Exit()
